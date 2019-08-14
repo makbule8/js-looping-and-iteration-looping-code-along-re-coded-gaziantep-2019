03bdea3adcb85e804b1579fd1fa1b1a53dbd57f6
makbule özbay <makbuleozbay8@gmail.com>
2019-08-14 13:00:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(badges) {
+  badges.forEach((name, i) => {
+    console.log(`Welcome ${name}! You are employee #${i + 1}.`)
+  })
+  return badges
+}
+
+function tailsNeverFails() {
+  tailsCount = 0
+
+  flip = Math.random()
+
+  while (flip >= 0.5) {
+    tailsCount ++
+    flip = Math.random()
+  }
+
+  return `You got ${tailsCount} tails in a row!`
+}
+© 2019 GitHub, Inc.
