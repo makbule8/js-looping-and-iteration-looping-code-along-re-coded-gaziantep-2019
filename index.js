7b4466a1f5b36347369c44b9e8c6d828e8cc8486
makbule özbay <makbuleozbay8@gmail.com>
2019-08-14 13:04:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+function writeCards (names, event){
+  const messages = [];
+  
+  for (let i = 0 ; i < names.length ; i++){
+    messages[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift.`;
+  }
+  
+  
+  return messages;
+}
+
+
+ function countdown(number){
+  number < 0 ? number *= -1 : number;
+  while (number >= 0 ){
+    console.log(number);
+    number--;
+  }
+}
